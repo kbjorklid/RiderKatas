01576da6e3362ad4207a3de6f6fa18a8b08180bf
Kalle Björklid <kalle.bjorklid@gmail.com>
2024-08-12 18:24:56 +0300

    Code generation & refactoring & dealing with warnings

---
+++ RiderKata/CodeGenerationAndRefactoring.cs
@@ -0,0 +1,25 @@
+﻿namespace RiderKata;
+
+public class CodeGenerationAndRefactoring
+{
+    private string _name;
+    
+    // Task 1: Generate a constructor using the generate menu
+    #region tip
+    // Alt + Insert
+    #endregion
+
+    // Task 2: in the constructor signature, add a second parameter 'int age', then
+    // using the context menu, generate a field for the age parameter.
+    #region tip
+    // Alt + Enter
+    #endregion
+    
+    // Task 3: Use 'refactor this' on constructor to change the order of the parameters
+    #region tip
+    // Ctrl + Shift + Alt + T
+    #endregion
+
+    // Task 4: Generate equality members using the generate menu
+
+}
---
+++ RiderKata/DealingWithWarnings.cs
@@ -0,0 +1,26 @@
+﻿namespace RiderKata;
+
+using System;
+
+
+// Task: Use 'next problem' key to go to next problem
+
+#region tip
+// f2
+#endregion
+
+public class DealingWithWarnings
+{
+    private string _name; // Task: use context menu Alt+Enter to generate a constructor 
+
+    // use next problem shortcut to go to next problem 
+
+    private void UnusedMethod() // Task: remove unused menu through context menu
+    {
+    }
+
+    public void Test()
+    {
+        string.Format("format {0}", 2); // Task: introduce variable using context menu
+    }
+}
